a5f990f8d98775d1c7f60c2f1f940745c2af72aa
mut4bor <matvei.molchanov2004@gmail.com>
2024-03-20 19:17:37 +0300

    hyprconf changes, added wlogout customization, added wlogout button to waybar

--- waybar/scripts/toggleAudio.sh
+++ waybar/scripts/toggleAudio.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# This script toggles audio output between headphones and speakers
 current_sink=$(pactl info | grep 'Default Sink' | awk '{print $3}')
 
- # My headphone device
-sink1="alsa_output.usb-Generic_BSX_USB_Audio_20210926172016-00.analog-stereo" # My speaker device
-sink2="alsa_output.pci-0000_30_00.4.analog-stereo" 
+# Get your devices writing "pamixer --list-sinks" in the terminal
+sink1="alsa_output.pci-0000_30_00.4.analog-stereo" # Headphones
+sink2="alsa_output.usb-Generic_BSX_USB_Audio_20210926172016-00.analog-stereo" # Speakers
 
-# Toggle between sinks
+# Toggle between devices
 if [ "$current_sink" == "$sink1" ]; then
     pactl set-default-sink "$sink2"
 else
